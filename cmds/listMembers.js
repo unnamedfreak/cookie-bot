1395c468591c2de81f45df16b266dfdbd264a6e7
unnamedfreak <32760644+unnamedfreak@users.noreply.github.com>
2020-04-16 03:46:11 +0530

    Bug fix
    Member Cache wouldn't update when 'listMembers' was used

--- cmds/listMembers.js
+++ cmds/listMembers.js
@@ -1,5 +1,6 @@
 module.exports.run = async (client, message, args) => {
     if(!message.member.roles.cache.has('426345039191474176')) return;
+    await message.guild.members.fetch();
     message.channel.send(`
         Total Members(+Bots): \`${message.guild.memberCount}\`
         Total Members: \`${message.guild.members.cache.filter(member => !member.user.bot).size}\`
